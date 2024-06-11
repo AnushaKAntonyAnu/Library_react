19d805c58757be6278e3fab077ae1c64b6d6a69e
AnushaKAntony <anushakantony@gmail.com>
2024-06-11 23:33:56 +0530

    project completed

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from './NavBar'
+import axios from 'axios'
 
 const AddBook = () => {
+    const[data,setData]=useState(
+        {
+            "title":"",
+            "category":"",
+            "publishedyear":"",
+            "authorname":"",
+            "description":"",
+            "distributor":"",
+            "publisher":"",
+            "price":""
+        }
+    )
+    const inputHandler=(event)=>{
+        setData({...data,[event.target.name]:event.target.value})
+    }
+    const readValue=()=>{
+        console.log(data)
+        axios.post("http://localhost:8081/add",data).then(
+            (response)=>{
+                console.log(response.data)
+                if (response.data.status=="success") {
+                    alert("successfully added")
+                } else {
+                    alert("error")
+                }
+            }
+        )
+    }
   return (
     <div>
         <NavBar/>
@@ -12,11 +41,11 @@ const AddBook = () => {
                     <div className="row g-3">
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Book Title</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='title' value={data.title} onChange={inputHandler} />
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Category</label>
-                            <select name=" " id="" className="form-control">
+                            <select id="" className="form-control" name='category' value={data.category} onChange={inputHandler}>
                                 <option value="Literature">Literature</option>
                                 <option value="Novel">Novel</option>
                                 <option value="FairyTale">FairyTale</option>
@@ -25,34 +54,31 @@ const AddBook = () => {
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Publish Year</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control"name='publishedyear' value={data.publishedyear} onChange={inputHandler} />
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Author Name</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='authorname' value={data.authorname} onChange={inputHandler} />
                         </div>
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                             <label htmlFor="" className="form-label">Description</label>
-                            <textarea name="" id="" className="form-control"></textarea>
+                            <textarea  id="" className="form-control" name='description' value={data.description} onChange={inputHandler}></textarea>
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Distributor Name</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='distributor' value={data.distributor} onChange={inputHandler} />
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Publisher Name</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='publisher' value={data.publisher} onChange={inputHandler}/>
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Price</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='price' value={data.price} onChange={inputHandler} />
                         </div>
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <label htmlFor="" className="form-label">Edition</label>
-                            <input type="text" className="form-control" />
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <button className="btn btn-success">Submit</button>
+                        
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                          <center> <button className="btn btn-success" onClick={readValue}>Submit</button></center> 
                         </div>
                     </div>
                 </div>
--- src/components/ViewallStudent.jsx
+++ src/components/ViewallStudent.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import NavBar from './NavBar'
+import axios from 'axios'
 
 const ViewallStudent = () => {
+    const[data,changeData]=useState([])
+    const fetchData=()=>{
+        axios.get("http://localhost:8081/view").then(
+            (response)=>{
+                changeData(response.data)
+            }
+        ).catch().finally()
+    }
+    useEffect(()=>{fetchData()},[])    
     return (
         <div><br></br>
             <NavBar/>
@@ -10,90 +20,40 @@ const ViewallStudent = () => {
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                         <table class="table table-striped table-hover">
-                            <table class="table">
+                           
                                 <thead>
                                     <tr>
                                     <th scope="col">sno</th>
                                         <th scope="col">Title</th>
+                                        <th scope="col">Category</th>
                                         <th scope="col">Author</th>
-                                        <th scope="col">PublishYear</th>
+                                        <th scope="col">Publishedyear</th>
+                                        <th scope="col">Description</th>
+                                        <th scope="col">Distributor</th>
+                                        <th scope="col">Publisher</th>
                                         <th scope="col">Price</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                        <th scope="row">1</th>
-                                        <td>Lilly</td>
-                                        <td>Ramkumar</td>
-                                        <td>2001</td>
-                                        <td>2001</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">2</th>
-                                        <td>nte jeevitham</td>
-                                        <td>Manu Mohan</td>
-                                        <td>1999</td>
-                                        <td>200</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">3</th>
-                                        <td>The moon</td>
-                                        <td>Roy</td>
-                                        <td>1998</td>
-                                        <td>456</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">4</th>
-                                        <td>Cloudy</td>
-                                        <td>manu</td>
-                                        <td>2001</td>
-                                        <td>345</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">5</th>
-                                        <td>Lilly</td>
-                                        <td>Ramkumar</td>
-                                        <td>2001</td>
-                                        <td>2001</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">6</th>
-                                        <td>Adujeevitham</td>
-                                        <td>Najeem</td>
-                                        <td>2010</td>
-                                        <td>199</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">7</th>
-                                        <td>Sachin</td>
-                                        <td>Sachin</td>
-                                        <td>2001</td>
-                                        <td>200</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">8</th>
-                                        <td>peaceful</td>
-                                        <td>Anusha</td>
-                                        <td>2022</td>
-                                        <td>2000</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">9</th>
-                                        <td>joyful</td>
-                                        <td>kumar</td>
-                                        <td>2002</td>
-                                        <td>450</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">10</th>
-                                        <td>Fairytale</td>
-                                        <td>Ram</td>
-                                        <td>2011</td>
-                                        <td>200</td>
-                                    </tr>
+                                    {data.map(
+                                        (value,index)=>{
+                                            return<tr>
+                                            <th scope="row">{index+1}</th>
+                                            <td>{value.title}</td>
+                                            <td>{value.category}</td>
+                                            <td>{value.authorname}</td>
+                                            <td>{value.publishedyear}</td>
+                                            <td>{value.description}</td>
+                                            <td>{value.distributor}</td>
+                                            <td>{value.publisher}</td>
+                                            <td>{value.price}</td>
+                                        </tr>
+                                       
+                                        }
+                                    )}
                                 </tbody>
                             </table>
-                        </table>
+                       
                     </div>
                 </div>
             </div>
